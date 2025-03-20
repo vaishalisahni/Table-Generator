f8d4a9c86286d55e7316822a29c97240f3fe5fc1
Vaishali Sahni <vaishalisahni10@gmail.com>
2025-03-20 09:59:30 +0530

    chnges

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const form=document.getElementById("GenerateTable");
 form.addEventListener("submit",function(event){
     event.preventDefault();
@@ -27,34 +26,4 @@ form.addEventListener("submit",function(event){
     
     
 
-=======
-const form=document.getElementById("GenerateTable");
-form.addEventListener("submit",function(event){
-    event.preventDefault();
-
-    const row=parseInt(document.getElementById("row-input").value);
-    const col=parseInt(document.getElementById("col-input").value);
-    
-    const table=document.createElement('table');
-
-    for( let i=0;i<row;i++)
-    {
-        const tr=document.createElement('tr');
-        for(let j=0;j<col;j++)
-        {
-            const td=document.createElement('td');
-            td.innerText='explorin';
-            tr.append(td);
-
-        }
-        table.append(tr);
-    }
-
-    const output=document.getElementById("output");
-    output.innerHTML="";
-    output.append(table);
-    
-    
-
->>>>>>> 83f97b0754b15d7e58fefd0010de2a8b9953c447
 })
